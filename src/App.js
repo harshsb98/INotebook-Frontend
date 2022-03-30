5685ae546fcdee3d6033a7fc3c4a0baf17555732
Harsh Sharma <harshsb98@gmail.com>
2022-03-30 13:30:20 +0530

    useLocation

--- src/App.js
+++ src/App.js
@@ -15,10 +15,12 @@ function App() {
     <NoteState>
     <Router>
     <NavBar />
+    <div className="container">
     <Routes>
     <Route exact path="/" element={<Home />} />
     <Route exact path="/about" element={<About />} />
     </Routes>
+    </div>
 </Router>
 </NoteState>
 </>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,23 +1,8 @@
 import noteContext from "./noteContext";
-import { useState } from "react";
 
 const NoteState =(props)=>{
-    const s1={
-        "name":"Harry",
-        "class":"5a"
-    }
-
-    const [state,setState]= useState(s1);
-    const update=()=>{
-        setTimeout(()=>{
-            setState({
-                "name":"larry",
-                "class":"10f"
-            })
-        },1000)
-    }
     return(
-        <noteContext.Provider value={{state,update}}>
+        <noteContext.Provider value={{}}>
             {props.children}
         </noteContext.Provider>
     )
