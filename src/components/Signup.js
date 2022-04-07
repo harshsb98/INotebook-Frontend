6a8702556034d7df4e5c0880f5ae20a29328cd93
Harsh Sharma <harshsb98@gmail.com>
2022-04-07 05:49:02 +0530

    Login Component

---
+++ src/components/Signup.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Signup = () => {
+  const [credentials, setcredentials] = useState({
+    name:"",
+    email: "",
+    password: "",
+    cpassword:""
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault(); //Preventing page from reloading
+  
+    const response = await fetch("http://localhost:5000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
+    const json = await response.json();
+    console.log(json);
+    if (json.success) {
+      //Save the authtoken and redirect
+      localStorage.setItem("token", json.authtoken);
+      history("/");
+    } else {
+      alert("Invalid Crdentials");
+    }
+  };
+  
+  const onChange = (e) => {
+    setcredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Name
+          </label>
+          <input
+            type="name"
+            className="form-control"
+            id="name"
+            aria-describedby="emailHelp"
+            onChange={onChange}
+            name="name"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onChange={onChange}
+            name="email"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            onChange={onChange}
+            name="password"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="cpassword" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="cpassword"
+            onChange={onChange}
+            name="cpassword"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
